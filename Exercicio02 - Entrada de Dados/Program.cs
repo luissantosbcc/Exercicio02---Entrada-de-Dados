0fd0578ee994406dd8abbc2e279d258289cdd6b9
luissantosbcc <luis.santos.bcc@gmail.com>
2020-05-26 08:32:29 -0300

    Commit inicial do projeto

---
+++ Exercicio02 - Entrada de Dados/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio02___Entrada_de_Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com, seu nome completo:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem sua casa?");
+            int quarto = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
+            string[] vet = Console.ReadLine().Split(' ');
+            string ultimoNome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(nome);
+            Console.WriteLine(quarto);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+            
+
+        }
+    }
+}
